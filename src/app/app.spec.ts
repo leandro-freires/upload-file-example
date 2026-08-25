2dedee442365e16cd9c690dfc2af417867fd05e5
Leandro Freires <rodrigues.freires@gmail.com>
2026-08-25 14:59:35 -0300

    refactor: Remoção do teste de renderização do Título não utilizado.

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -4,21 +4,13 @@ import { App } from './app';
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
-    })
-      .compileComponents();
+      imports: [App]
+    }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, upload-file-example');
   });
 });
